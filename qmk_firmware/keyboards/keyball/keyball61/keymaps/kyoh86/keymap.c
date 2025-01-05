ca726298bb7d0224d79b6050eb3078a337841137
kyoh86 <me@kyoh86.dev>
2025-01-05 20:27:21 +0900

    fix: remove unneccessary comma at eol

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/kyoh86/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/kyoh86/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,           KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,                                                   KC_F7,           KC_F8,           KC_F9,           KC_P7,           KC_P8,           KC_P9,
     KC_LCTL,         KC_A,            KC_S,            KC_D,            KC_F,            KC_G,                                                   KC_F4,           KC_F5,           KC_F6,           KC_P4,           KC_P5,           KC_P6,
     KC_LSFT,         KC_Z,            KC_X,            KC_C,            KC_V,            KC_B,            KC_LCTL,              KC_ESC,          KC_F1,           KC_F2,           KC_F3,           KC_P1,           KC_P2,           KC_P3,
-    TO(0),           KC_6,            KC_7,            KC_8,            KC_ESC,          KC_SPC,          KC_LSFT,              KC_BSPC,         KC_ENT,          KC_NO,           KC_NO,           KC_NO,           KC_P0,           KC_DOT,
+    TO(0),           KC_6,            KC_7,            KC_8,            KC_ESC,          KC_SPC,          KC_LSFT,              KC_BSPC,         KC_ENT,          KC_NO,           KC_NO,           KC_NO,           KC_P0,           KC_DOT
   ),
 
   [3] = LAYOUT_universal(
@@ -88,5 +88,4 @@ void oledkit_render_info_user(void) {
 }
 #endif
 
-/* vim:colorcolumn=5,22,39,56,73,90,107,129,146,163,180,197,214,231,248
- */
+// vim:colorcolumn=5,22,39,56,73,90,107,129,146,163,180,197,214,231,248
