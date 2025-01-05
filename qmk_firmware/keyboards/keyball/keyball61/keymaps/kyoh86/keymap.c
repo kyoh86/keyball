fe754edfebd3edc406a3de947ebeae1dbdec5054
kyoh86 <me@kyoh86.dev>
2025-01-05 21:53:32 +0900

    try to put MS_WHLU/D on maps for game

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/kyoh86/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/kyoh86/keymap.c
@@ -54,8 +54,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // For game
   [2] = LAYOUT_universal(
-    KC_NO,           KC_1,            KC_2,            KC_3,            KC_4,            KC_5,                                                   KC_6,            KC_7,            KC_8,            KC_9,            KC_0,            KC_NO,
-    KC_NO,           KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,                                                   KC_F7,           KC_F8,           KC_F9,           KC_P7,           KC_P8,           KC_P9,
+    MS_WHLU,         KC_1,            KC_2,            KC_3,            KC_4,            KC_5,                                                   KC_6,            KC_7,            KC_8,            KC_9,            KC_0,            KC_NO,
+    MS_WHLD,         KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,                                                   KC_F7,           KC_F8,           KC_F9,           KC_P7,           KC_P8,           KC_P9,
     KC_LALT,         KC_A,            KC_S,            KC_D,            KC_F,            KC_G,                                                   KC_F4,           KC_F5,           KC_F6,           KC_P4,           KC_P5,           KC_P6,
     KC_LSFT,         KC_Z,            KC_X,            KC_C,            KC_V,            KC_B,            KC_LCTL,              KC_ESC,          KC_F1,           KC_F2,           KC_F3,           KC_P1,           KC_P2,           KC_P3,
     TO(0),           KC_6,            KC_7,            KC_8,            KC_ESC,          KC_SPC,          KC_LSFT,              KC_BSPC,         KC_ENT,          KC_NO,           KC_NO,           KC_NO,           KC_P0,           KC_DOT
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+    keyball_set_scroll_mode(get_highest_layer(state) == 1);
     return state;
 }
 
