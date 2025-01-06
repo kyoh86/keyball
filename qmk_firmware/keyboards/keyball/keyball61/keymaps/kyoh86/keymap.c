ec4b1b9fe97d4247c292e4660f89ed1712833844
kyoh86 <me@kyoh86.dev>
2025-01-06 18:16:05 +0900

    rearrange Function keys and change scroll mode on L3->L1

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/kyoh86/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/kyoh86/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Num pad, function keys, arrow keys, mouse buttons, and scroll buttons
   [1] = LAYOUT_universal(
-    KC_F1,           KC_F2,           KC_F3,           KC_F4,           KC_F5,           KC_F6,                                                  KC_F7,           KC_F8,           KC_F9,           KC_F10,          KC_F11,          KC_F12,
+    KC_F11,          KC_F1,           KC_F2,           KC_F3,           KC_F4,           KC_F5,                                                  KC_F6,           KC_F7,           KC_F8,           KC_F9,           KC_F10,          KC_F12,
     KC_TAB,          KC_NO,           KC_P7,           KC_P8,           KC_P9,           KC_NO,                                                  KC_HOME,         KC_LEFT,         KC_UP,           KC_RGHT,         KC_END,          KC_NO,
     KC_LALT,         KC_NO,           KC_P4,           KC_P5,           KC_P6,           KC_NO,                                                  KC_PGUP,         KC_BTN1,         KC_DOWN,         KC_BTN2,         KC_PGDN,         KC_LALT,
     KC_LSFT,         KC_NO,           KC_P1,           KC_P2,           KC_P3,           KC_NO,           KC_NO,                KC_NO,           KC_NO,           KC_NO,           KC_BTN3,         KC_NO,           KC_NO,           KC_LSFT,
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+    keyball_set_scroll_mode(get_highest_layer(state) == 1);
     return state;
 }
 
