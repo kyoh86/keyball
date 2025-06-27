064ae3b34ff749f07a5b8479b40dd7f2ccaff7db
kyoh86 <me@kyoh86.dev>
2025-06-27 19:56:28 +0900

    change keymap

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/kyoh86/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/kyoh86/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_EQUAL,        KC_1,            KC_2,            KC_3,            KC_4,            KC_5,                                                   KC_6,            KC_7,            KC_8,            KC_9,            KC_0,            KC_MINS,
     KC_GRAVE,        KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,                                                   KC_Y,            KC_U,            KC_I,            KC_O,            KC_P,            KC_BSLS,
-    KC_LALT,         KC_A,            KC_S,            KC_D,            KC_F,            KC_G,                                                   KC_H,            KC_J,            KC_K,            KC_L,            KC_SCLN,         LALT_T(KC_QUOTE),
+    KC_LALT,         KC_A,            KC_S,            KC_D,            KC_F,            KC_G,                                                   KC_H,            KC_J,            KC_K,            KC_L,            KC_SCLN,         RALT_T(KC_QUOTE),
     KC_LSFT,         KC_Z,            KC_X,            KC_C,            KC_V,            KC_B,            LCTL_T(KC_ESC),       LCTL_T(KC_ESC),  KC_N,            KC_M,            KC_COMM,         KC_DOT,          KC_SLSH,         KC_LSFT,
     KC_LNG2,         KC_LGUI,         KC_LBRC,         KC_RBRC,         LT(3,KC_ESC),    LT(2,KC_SPC),    LT(1,KC_TAB),         KC_BSPC,         LT(1,KC_ENT),    KC_NO,           KC_NO,           KC_NO,           KC_LGUI,         KC_LNG1
   ),
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_universal(
     KC_F11,          KC_F1,           KC_F2,           KC_F3,           KC_F4,           KC_F5,                                                  KC_F6,           KC_F7,           KC_F8,           KC_F9,           KC_F10,          KC_F12,
     KC_TAB,          KC_NO,           KC_P7,           KC_P8,           KC_P9,           KC_NO,                                                  KC_HOME,         KC_LEFT,         KC_UP,           KC_RGHT,         KC_END,          KC_NO,
-    KC_LALT,         KC_NO,           KC_P4,           KC_P5,           KC_P6,           KC_NO,                                                  KC_PGUP,         KC_BTN1,         KC_DOWN,         KC_BTN2,         KC_PGDN,         KC_LALT,
+    KC_LALT,         KC_NO,           KC_P4,           KC_P5,           KC_P6,           KC_NO,                                                  KC_PGUP,         KC_BTN1,         KC_DOWN,         KC_BTN2,         KC_PGDN,         KC_RALT,
     KC_LSFT,         KC_NO,           KC_P1,           KC_P2,           KC_P3,           KC_NO,           KC_NO,                KC_NO,           KC_NO,           KC_NO,           KC_BTN3,         KC_NO,           KC_NO,           KC_LSFT,
     TO(0),           KC_LGUI,         KC_P0,           KC_DOT,          KC_NO,           KC_NO,           KC_NO,                KC_DEL,          KC_ENT,          KC_NO,           KC_NO,           KC_NO,           KC_LGUI,         KC_NO
   ),
@@ -58,13 +58,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,          KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,                                                   KC_F7,           KC_F8,           KC_F9,           KC_P7,           KC_P8,           KC_P9,
     KC_LALT,         KC_A,            KC_S,            KC_D,            KC_F,            KC_G,                                                   KC_F4,           KC_F5,           KC_F6,           KC_P4,           KC_P5,           KC_P6,
     KC_LSFT,         KC_Z,            KC_X,            KC_C,            KC_V,            KC_B,            KC_LCTL,              KC_ESC,          KC_F1,           KC_F2,           KC_F3,           KC_P1,           KC_P2,           KC_P3,
-    TO(0),           KC_6,            KC_7,            KC_8,            KC_ESC,          KC_SPC,          KC_LSFT,              KC_BSPC,         KC_ENT,          KC_NO,           KC_NO,           KC_NO,           KC_P0,           KC_DOT
+    TO(0),           KC_6,            KC_7,            KC_8,            KC_ESC,          KC_SPC,          KC_LSFT,              KC_BSPC,         KC_ENT,          KC_NO,           KC_NO,           KC_NO,           KC_P0,           KC_PRINT_SCREEN
   ),
 
   [3] = LAYOUT_universal(
     TO(1),           KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_NO,                                                  KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_NO,
     TO(2),           KC_NO,           KC_UP,           KC_NO,           KC_NO,           KC_NO,                                                  KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_NO,
-    KC_LALT,         KC_LEFT,         KC_DOWN,         KC_RIGHT,        KC_NO,           KC_ENT,                                                 KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_LALT,
+    KC_LALT,         KC_LEFT,         KC_DOWN,         KC_RIGHT,        KC_NO,           KC_ENT,                                                 KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_RALT,
     KC_LSFT,         KC_NO,           CPI_D1K,         CPI_D100,        CPI_I100,        CPI_I1K,         KBC_SAVE,             KBC_RST,         SCRL_DVI,        SCRL_DVD,        KC_NO,           KC_NO,           KC_NO,           KC_LSFT,
     TO(0),           KC_LGUI,         KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_NO,                KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_LGUI,         KC_NO
   ),
