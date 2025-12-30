a4b4061ad08d5bfa52f6d821789fa086e5e3b7c7
kyoh86 <me@kyoh86.dev>
2025-12-30 14:21:31 +0900

    Put Print-Screen key

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/kyoh86/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/kyoh86/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Num pad, function keys, arrow keys, mouse buttons, and scroll buttons
   [1] = LAYOUT_universal(
     KC_F11,          KC_F1,           KC_F2,           KC_F3,           KC_F4,           KC_F5,                                                  KC_F6,           KC_F7,           KC_F8,           KC_F9,           KC_F10,          KC_F12,
-    KC_TAB,          KC_NO,           KC_P7,           KC_P8,           KC_P9,           KC_NO,                                                  KC_HOME,         KC_LEFT,         KC_UP,           KC_RGHT,         KC_END,          KC_NO,
+    KC_TAB,          KC_NO,           KC_P7,           KC_P8,           KC_P9,           KC_PSCR,                                                KC_HOME,         KC_LEFT,         KC_UP,           KC_RGHT,         KC_END,          KC_NO,
     KC_LALT,         KC_NO,           KC_P4,           KC_P5,           KC_P6,           KC_NO,                                                  KC_PGUP,         KC_BTN1,         KC_DOWN,         KC_BTN2,         KC_PGDN,         KC_RALT,
     KC_LSFT,         KC_NO,           KC_P1,           KC_P2,           KC_P3,           KC_NO,           KC_NO,                KC_NO,           KC_NO,           KC_NO,           KC_BTN3,         KC_NO,           KC_NO,           KC_LSFT,
     TO(0),           KC_LGUI,         KC_P0,           KC_DOT,          KC_NO,           KC_NO,           KC_NO,                KC_DEL,          KC_ENT,          KC_NO,           KC_NO,           KC_NO,           KC_LGUI,         KC_NO
