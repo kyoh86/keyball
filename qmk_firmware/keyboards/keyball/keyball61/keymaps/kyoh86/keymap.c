706984f2dfb30ba26ffd56055231905aeb762831
kyoh86 <me@kyoh86.dev>
2026-01-30 14:14:30 +0900

    fix: change BS to DEL on layer2

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/kyoh86/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/kyoh86/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,          KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,                                                   KC_F7,           KC_F8,           KC_F9,           KC_P7,           KC_P8,           KC_P9,
     KC_LALT,         KC_A,            KC_S,            KC_D,            KC_F,            KC_G,                                                   KC_F4,           KC_F5,           KC_F6,           KC_P4,           KC_P5,           KC_P6,
     KC_LSFT,         KC_Z,            KC_X,            KC_C,            KC_V,            KC_B,            KC_LCTL,              KC_ESC,          KC_F1,           KC_F2,           KC_F3,           KC_P1,           KC_P2,           KC_P3,
-    TO(0),           KC_6,            KC_7,            KC_8,            KC_ESC,          KC_SPC,          KC_LSFT,              KC_BSPC,         KC_ENT,          KC_NO,           KC_NO,           KC_NO,           KC_P0,           KC_PRINT_SCREEN
+    TO(0),           KC_6,            KC_7,            KC_8,            KC_ESC,          KC_SPC,          KC_LSFT,              KC_DEL,          KC_ENT,          KC_NO,           KC_NO,           KC_NO,           KC_P0,           KC_PRINT_SCREEN
   ),
 
   [3] = LAYOUT_universal(
